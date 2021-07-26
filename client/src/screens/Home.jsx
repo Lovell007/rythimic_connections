8d442a403f8294cac895de8800cf62a1270862bc
james lovell <jameslovell@jamess-MacBook-Air.local>
2021-07-26 10:25:05 -0500

    some home CSS

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -1,47 +1,47 @@
-import React, {useState} from 'react'
-import { Link } from 'react-router-dom'
-import '../assets/css/Home.css'
-import headerImage from '../assets/images/Music.png'
-import Modal from '../components/Modal'
-import PlaylistCreate from './PlaylistCreate'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../assets/css/Home.css';
+import headerImage from '../assets/images/Music.png';
+import Modal from '../components/Modal';
+import PlaylistCreate from './PlaylistCreate';
 
 export default function Home(props) {
-  const { currentUser, userPlaylists } = props
+  const { currentUser, userPlaylists } = props;
   const [modal, setModal] = useState(false);
-  
+
   return (
     <div>
-      <img className="homeImg" src={headerImage}/>
+      <img className="homeImg" src={headerImage} />
       {!currentUser ? (
         <>
-        <div className='cyop' onClick={() => setModal(true)}>
-          Create Your Own Playlist!!!
-        </div>
-        <Modal modal={modal} setModal={setModal}>
-          <PlaylistCreate />
-        </Modal>
+          <div className="cyop" onClick={() => setModal(true)}>
+            Create Your Own Playlist!!!
+          </div>
+          <Modal modal={modal} setModal={setModal}>
+            <PlaylistCreate />
+          </Modal>
         </>
       ) : (
-        <>
-        <div className='cyop' onClick={() => setModal(true)}>
-          Create Your Own Playlist
-        </div>
-        <Modal modal={modal} setModal={setModal} className='createModal'>
-          <PlaylistCreate />
-            </Modal>
-            <div>
-            {userPlaylists.map((playlist) => (
-            <div key={playlist.id}>
-                <Link to={`/playlists/${playlist.id}`}>
-                <p>{playlist.name}</p>
-                </Link>
+        <div>
+          <p className="playlistsSecName">Playlists</p>
+          <div className="homeList">
+            <div className="cyop" onClick={() => setModal(true)}>
+              Create Your Own Playlist
             </div>
-          ))}
+            <Modal modal={modal} setModal={setModal}>
+              <PlaylistCreate />
+            </Modal>
+            {userPlaylists.map(playlist => (
+              <div key={playlist.id}>
+                <Link to={`/playlists/${playlist.id}`}>
+                  <div className="homePLimg">Place holder</div>
+                  <p className="homeName">{playlist.name}</p>
+                </Link>
+              </div>
+            ))}
+          </div>
         </div>
-        </>
-      )}      
+      )}
     </div>
-  )
+  );
 }
-
-
--- client/src/screens/PlaylistDetails.jsx
+++ client/src/screens/PlaylistDetails.jsx
@@ -1,36 +1,41 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { getOnePlaylist } from '../services/playlists'
-import { BsThreeDots } from 'react-icons/bs'
-import { IconContext } from 'react-icons/'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getOnePlaylist } from '../services/playlists';
+import { BsThreeDots } from 'react-icons/bs';
+import { IconContext } from 'react-icons/';
+import Modal from '../components/Modal';
 
 export default function PlaylistDetails() {
-  const [playlist, setPlaylist] = useState(null)
-  const { id } = useParams()
+  const [modal, setModal] = useState(false);
+  const [playlist, setPlaylist] = useState(null);
+  const { id } = useParams();
   console.log(id);
-  
+
   useEffect(() => {
     const fetchPlaylist = async () => {
-      const playlistData = await getOnePlaylist(id)
-      setPlaylist(playlistData)
-    }
-    fetchPlaylist()
-  }, [id])
+      const playlistData = await getOnePlaylist(id);
+      setPlaylist(playlistData);
+    };
+    fetchPlaylist();
+  }, [id]);
 
   return (
     <div>
-      <h3 className='playlistTitle'>{playlist?.name}</h3>
-      {playlist?.songs.map((song) => (
-        <div className='songRows'>
-          <img className='rowSongImg' src={song.image_url} />
-          <p className='rowSongName' key={song.id}>{song.name}</p>
+      <h3 className="playlistTitle">{playlist?.name}</h3>
+      {playlist?.songs.map(song => (
+        <div className="songRows">
+          <img className="rowSongImg" src={song.image_url} />
+          <p className="rowSongName" key={song.id}>
+            {song.name}
+          </p>
           <IconContext.Provider value={{ size: 50 }}>
             <div>
-              <BsThreeDots />
+              <BsThreeDots onClick={() => setModal(true)} />
             </div>
           </IconContext.Provider>
+          <Modal></Modal>
         </div>
       ))}
     </div>
-  )
+  );
 }
