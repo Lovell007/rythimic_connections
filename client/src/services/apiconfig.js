9ebdafe1accc3147dc8299a754b0f0ea565817d1
james lovell <jameslovell@jamess-MacBook-Air.local>
2021-07-27 14:44:55 -0500

    apiconfig production url fixed

--- client/src/services/apiconfig.js
+++ client/src/services/apiconfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = process.env.NODE_ENV === 'production' ? 'https://git.heroku.com/rhythmic-connections-api.git' :'http://localhost:3000'
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://rhythmic-connections-api.herokuapp.com/' :'http://localhost:3000'
 
 const api = axios.create({
   baseURL: baseUrl
